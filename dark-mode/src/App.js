955b91ff1db03575b358cee946e7e97c4d1b87c9
harsh-100 <26harshagarwal@gmail.com>
2022-05-13 10:22:49 +0530

    Dark - Mode page

---
+++ dark-mode/src/App.js
@@ -0,0 +1,43 @@
+import React,{useState,useEffect} from 'react';
+
+import './App.css';
+
+function App() {
+
+  //To make the state same afer refreshing the page 
+  //I have used the useEffect hook for this 
+  const getMode = ()=>{
+    return JSON.parse(localStorage.getItem("mode")) || false
+  }
+  const [dark, setdark] = useState(getMode());
+ 
+ useEffect(()=>{
+   localStorage.setItem("mode",JSON.stringify(dark))
+ 
+ },[dark])
+ 
+  
+  return (
+    <div className={dark?"App dark-mode":"App"}>
+      <div className="nav">
+            <label className="switch">
+            <input 
+            type="checkbox"
+            checked = {dark}
+            onChange = {()=>setdark(!dark)}
+            />
+            <span className="slider round"></span>
+          </label>
+      </div>
+      <div className="content">
+      <h1>{dark?"Dark Mode":"Light Mode"} </h1>
+      <p style={{fontSize: "25px"}}>want to see the magic ? Toggle to Dark Mode</p>
+      </div>
+      
+      
+    
+    </div>
+  );
+}
+
+export default App;
